b1478306b29e2866f9f97a38fc269215ad08756a
wisthn <wisthn@gmatc.matc.edu>
2020-10-14 12:10:51 -0500

    created mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import connectDatabase from './config/db';
 
 const app = express();
+
+connectDatabase();
 
 app.get('/', (req, res) => {
     res.send('http get response sent to root api endpoint');
